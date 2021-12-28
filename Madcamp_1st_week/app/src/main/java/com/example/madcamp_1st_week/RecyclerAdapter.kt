176ba8efa35e69e2964ccb342d15bab3a9cc86a7
이주현 <juhyun@ijuhyeon-ui-MacBookAir.local>
2021-12-28 21:45:19 +0900

    Activity

---
+++ Madcamp_1st_week/app/src/main/java/com/example/madcamp_1st_week/RecyclerAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.madcamp_1st_week
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class RecyclerAdapter (private val context: Context): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+    var datas = mutableListOf<FriendItem>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_recyclerview,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = datas.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(datas[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val txtName: TextView = itemView.findViewById(R.id.name)
+        private val txtMsg: TextView = itemView.findViewById(R.id.message)
+
+        fun bind(item: FriendItem) {
+            txtName.text = item.name
+            txtMsg.text = item.message
+
+        }
+
+    }
+}
