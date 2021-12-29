5fc55b22babd407a0e08fb6803650fe3732c4c8d
이주현 <juhyun@ijuhyeon-ui-MacBookAir.local>
2021-12-29 15:32:25 +0900

    Recursive problem solved

--- Madcamp_1st_week/app/src/main/java/com/example/madcamp_1st_week/FriendItem.kt
+++ Madcamp_1st_week/app/src/main/java/com/example/madcamp_1st_week/FriendItem.kt
@@ -3,19 +3,9 @@ package com.example.madcamp_1st_week
 class FriendItem (var _name: String, var _phone: String, var _info: String)
 {
     var name:String = _name
-        get() = this.name
-        set(nickname: String) {
-            field = nickname
-        }
+
     var phone: String = _phone
-        get() = this.phone
-        set(phone: String) {
-            field = phone
-        }
+
     var info: String = _info
-        get() = this.info
-        set(info: String) {
-            field = info
-        }
 
 }
